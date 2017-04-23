6bc38bbfdfa4dcf76d4379c6671c587d0a342c84
Jonathan Elliott <jonathan_elliott16@yahoo.com>
2017-04-23 16:15:48 -0500

    First Commit

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public float autoLoadNextLevelAfter;
+
+	void Start(){
+		Invoke ("LoadNextLevel",autoLoadNextLevelAfter);
+	}
+	
+	public void LoadLevel(string name){
+		Debug.Log("Level Load Request for: "+name);
+		Application.LoadLevel(name);
+	}
+	
+	private void LoadNextLevel(){
+		Application.LoadLevel(Application.loadedLevel+1);
+	}
+	
+	public void QuitLevel(){
+		Debug.Log("Quit Game Request");
+		//Application.Quit();
+	}
+}
