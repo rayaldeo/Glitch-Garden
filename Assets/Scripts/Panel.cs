0615c5c48fe598fb3c542528e9f7c250f6fc3962
Jonathan Elliott <jonathan_elliott16@yahoo.com>
2017-04-26 23:17:57 -0500

    Added a Fade In Effect to each scene

---
+++ Assets/Scripts/Panel.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Panel : MonoBehaviour {
+
+	public float fadeInTime;
+	private Image fadePanel;
+	private Color currentColor = Color.black;
+
+	// Use this for initialization
+	void Start () {
+		fadePanel = GetComponent<Image>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Time.timeSinceLevelLoad < fadeInTime){
+			//Fade In
+			FadeInTime();
+		}else{
+			gameObject.SetActive(false);
+		}
+	
+	}
+	
+	
+	void FadeInTime(){
+		float alphaChange = Time.deltaTime/fadeInTime;
+		currentColor.a -= alphaChange;
+		fadePanel.color = currentColor;
+	}
+}
