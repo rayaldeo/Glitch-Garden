a8d8f8b0f4fbd873e5432f65cac18d44ced89b5a
Jonathan Elliott <jonathan_elliott16@yahoo.com>
2017-05-26 15:50:15 -0500

    Added Colliders to all Defenders and Attackers,and RigidBody to all Attackers only

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -9,12 +9,19 @@ public class Attacker : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		Rigidbody2D myRigid = gameObject.AddComponent<Rigidbody2D>();
+		myRigid.isKinematic=true;
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		transform.Translate(Vector3.left*walkSpeed*Time.deltaTime);
+	
+	}
+	
+	void OnTriggerEnter2D(){
+		Debug.Log (name + "trigger Enter for Attackers");
 	
 	}
 }
---
+++ Assets/Scripts/Defender.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Defender : MonoBehaviour {
+
+	void OnTriggerEnter2D(){
+		Debug.Log (name + "trigger Enter for Defender");
+	}
+}
