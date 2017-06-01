dd63bea7797abfbd0176bc750eb6f680a86f3b45
Jonathan Elliott <jonathan_elliott16@yahoo.com>
2017-05-31 23:05:17 -0500

    Got the defenders firing again,this time with Scripts and Animation Events for better control

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shooter : MonoBehaviour {
+	public GameObject projectile,projectileParent,gun;
+	
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	private void Fire(){
+		GameObject newProjectile =Instantiate(projectile) as GameObject;
+		newProjectile.transform.parent =projectileParent.transform;
+		newProjectile.transform.position =gun.transform.position;
+	}
+}
