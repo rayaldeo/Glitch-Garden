fa22951ef77ae8c8cda8f17d431d01e762c68cd5
Jonathan Elliott <jonathan_elliott16@yahoo.com>
2017-06-03 21:42:56 -0500

    Finishing up the Defenders

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -16,5 +16,14 @@ public class Projectile : MonoBehaviour {
 		transform.Translate(Vector3.right *speed *Time.deltaTime);
 	}
 	
+	void OnTriggerEnter2D(Collider2D collider){
+		Attacker attacker = collider.gameObject.GetComponent<Attacker>();
+		HealthController health = collider.gameObject.GetComponent<HealthController>();
+		if(attacker && health){
+			health.DealDamage(damage);
+			Destroy(gameObject);
+		}
+	}
+	
 	
 }
