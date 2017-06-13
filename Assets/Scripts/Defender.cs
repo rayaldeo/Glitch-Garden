96a2ea801e189eb770a24db2c658ab7111f76e02
Jonathan Elliott <jonathan_elliott16@yahoo.com>
2017-06-12 21:33:02 -0500

    Added a Text Element to  hold the total amount of stars,star Defender  now gives off stars

--- Assets/Scripts/Defender.cs
+++ Assets/Scripts/Defender.cs
@@ -1,8 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-
 public class Defender : MonoBehaviour {
+
+	private StarDisplay starDisplay;
+	public int defenderStarCost;
+	
+	void Start(){
+		starDisplay = GameObject.FindObjectOfType<StarDisplay>();
+	}
 
 	void OnTriggerEnter2D(){
 		Debug.Log (name + "trigger Enter for Defender");
@@ -10,5 +16,10 @@ public class Defender : MonoBehaviour {
 	
 	void Update(){
 		
+	}
+	
+	public void AddStars(int amount){
+		Debug.Log(amount+" stars has been added");
+		starDisplay.AddStars(amount);
 	}
 }
---
+++ Assets/Scripts/StarDisplay.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent (typeof(Text))]
+public class StarDisplay : MonoBehaviour {
+
+	private int allStars;
+	private Text totalStarsText;
+
+	// Use this for initialization
+	void Start () {
+		totalStarsText= GetComponent<Text>();
+	}
+	
+		
+	public void AddStars(int amount){
+		print (amount+ " stars added to display");
+		allStars +=amount;
+		totalStarsText.text =allStars.ToString();
+		
+	}
+	
+	public void UseStars(int amount){
+		print (amount+ " stars subtracted from the display");
+		allStars -=amount;
+		totalStarsText.text =allStars.ToString();
+	}
+}
